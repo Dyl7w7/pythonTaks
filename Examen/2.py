8bef2adaef3dd0b9205a5055845dfc730e9ad2ba
Dylan <dilanstivel9@gmail.com>
2024-02-26 13:02:34 -0500

    Examen Python

---
+++ Examen/2.py
@@ -0,0 +1,28 @@
+op = 0
+while (op == 0):
+    totalPagar = 0
+    montoAdicional = 0
+    kmExceso = 0
+    totalMonto = 0
+    montoFijo = 300000
+    km = int(input("Ingrese los kilómetros recorridos: "))
+    if(km > 500 and km <= 1000):
+        kmExceso = km - 500
+        montoAdicional = 15000
+        totalMonto = kmExceso * montoAdicional
+        totalPagar = totalMonto + montoFijo
+    elif(km > 1000):
+        kmExceso = km - 1000
+        totalMontox500 = 500 * 15000
+        montoAdicional = 10000
+        totalMonto = kmExceso * montoAdicional + totalMontox500
+        totalPagar = totalMonto + montoFijo
+    else:
+        totalPagar = montoFijo
+
+    print("El monto adicional por exceso de kilómetros es: $",totalMonto)
+    print("El total a pagar es: $",totalPagar)
+    print()
+    print("###########################################################")
+    print("¿Desea continuar? \n 0. Si  /  1. No")
+    op = int(input())
