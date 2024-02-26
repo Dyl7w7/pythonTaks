a0264aa03fc4a7e577be76aef97d7bd1ed310e80
Dylan <dilanstivel9@gmail.com>
2024-02-26 14:10:24 -0500

    Calculo del impuesto

--- Examen/2.py
+++ Examen/2.py
@@ -11,17 +11,21 @@ while (op == 0):
         montoAdicional = 15000
         totalMonto = kmExceso * montoAdicional
         totalPagar = totalMonto + montoFijo
+        iva = totalPagar * 0.2
     elif(km > 1000):
         kmExceso = km - 1000
         totalMontox500 = 500 * 15000
         montoAdicional = 10000
         totalMonto = kmExceso * montoAdicional + totalMontox500
         totalPagar = totalMonto + montoFijo
+        iva = totalPagar * 0.2
     else:
         totalPagar = montoFijo
+        iva = totalPagar * 0.2
 
     print("El monto adicional por exceso de kilómetros es: $",totalMonto)
     print("El total a pagar es: $",totalPagar)
+    print("El impuesto equivale a: $",iva)
     print()
     print("###########################################################")
     print("¿Desea continuar? \n 0. Si  /  1. No")
